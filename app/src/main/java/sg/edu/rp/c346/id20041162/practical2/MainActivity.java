cd1910893ecd6e1df88b428bb71fc4dd43feddbc
SeanAwh <20041162@myrp.edu.sg>
2021-06-01 14:52:01 +0800

    commit1

--- app/src/main/java/sg/edu/rp/c346/id20041162/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20041162/practical2/MainActivity.java
@@ -3,8 +3,11 @@ package sg.edu.rp.c346.id20041162.practical2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton catBtn, cowBtn, crocodileBtn, camelBtn, chickenBtn, dogBtn, deerBtn, donkeyBtn, duckBtn, dolphinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
